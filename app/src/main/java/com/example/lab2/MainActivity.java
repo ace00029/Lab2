3a43a5fa706f4acc15f4744feea52cfabd44e7c7
ace00029 <alvaro.castro@tprs.stud.vu.lt>
2022-10-17 03:35:45 +0300

    Task implementation

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.lab2;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import utils.TextCounter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spSelectOptions;
+    private EditText edPhrase;
+    private TextView tvMain;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.edPhrase = findViewById(R.id.edPhrase);
+        this.tvMain = findViewById(R.id.tvMain);
+
+        this.spSelectOptions = (Spinner) findViewById(R.id.spSelectOptions);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.selection_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spSelectOptions.setAdapter(adapter);
+
+    }
+
+    public void onBrnCountClick(View view) {
+        String selectedOption = this.spSelectOptions.getSelectedItem().toString();
+        String defaultCharsSelectedOption = getResources().getString(R.string.chars_selection);
+        String defaultWordsSelectedOption = getResources().getString(R.string.words_selection);
+
+        String userImput = this.edPhrase.getText().toString();
+        Boolean isEmpty = userImput.isEmpty();
+
+        if (!isEmpty) {
+            if (selectedOption.equals(defaultCharsSelectedOption)) {
+                String userImputText = this.edPhrase.getText().toString();
+                String charsCount = TextCounter.getCharsCount(userImputText);
+                this.tvMain.setText(charsCount);
+            } else {
+                if (selectedOption.equals(defaultWordsSelectedOption)) {
+                    String userImputText = this.edPhrase.getText().toString();
+                    String wordsCount = TextCounter.getWordsCount(userImputText);
+                    this.tvMain.setText(wordsCount);
+                }
+            }
+        } else {
+            Toast.makeText(this, "Enter any input", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
---
+++ app/src/main/java/utils/TextCounter.java
@@ -0,0 +1,40 @@
+package utils;
+
+public class TextCounter {
+    public static String getCharsCount(String input) {
+        return String.valueOf(input.length());
+    }
+    public static String getWordsCount(String input){
+        int wordCont = 0;
+
+        int i = 0;
+        char[] imputArray = input.toCharArray();
+        boolean end = false;
+        while (i < imputArray.length && !end) {
+
+            if (i < imputArray.length){
+                while (!end && imputArray[i] == ' ' || imputArray[i] == '.'
+                                        || imputArray[i] == ',') {
+                    if (i < imputArray.length-1)
+                        i++;
+                    else end=true;
+                }
+            }
+            if (i < imputArray.length && imputArray[i] != ' ' && imputArray[i] != '.'
+                    && imputArray[i] != ',') {
+                wordCont++;
+            }
+            if (i < imputArray.length) {
+                while (!end && imputArray[i] != ' ' && imputArray[i] != '.'
+                        && imputArray[i] != ',') {
+                    if (i < imputArray.length-1)
+                        i++;
+                    else end=true;
+                }
+            }
+        }
+
+
+        return String.valueOf(wordCont);
+    }
+}
